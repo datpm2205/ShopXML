b4b6a0435f0814302d8b97d5d64a1282045faef9
dotrungduc <duc010298@gmail.com>
2020-03-28 14:45:19 +0700

    common source front end

--- client/src/app/app-routing.module.ts
+++ client/src/app/app-routing.module.ts
@@ -1,11 +1,19 @@
 import { NgModule } from '@angular/core';
-import { Routes, RouterModule } from '@angular/router';
+import { Routes, RouterModule, PreloadAllModules } from '@angular/router';
+import { Shell } from './shell/shell.service';
 
 
-const routes: Routes = [];
+const routes: Routes = [
+  Shell.childRoutes([
+    { path: '', loadChildren: () => import('./home/home.module').then(m => m.HomeModule) },
+  ]),
+
+  // Fallback when no prior route is matched
+  { path: '**', redirectTo: '', pathMatch: 'full' }
+];
 
 @NgModule({
-  imports: [RouterModule.forRoot(routes)],
+  imports: [RouterModule.forRoot(routes, { preloadingStrategy: PreloadAllModules })],
   exports: [RouterModule]
 })
 export class AppRoutingModule { }
