d8fefab9f00ea25d82f34e3d2d1fe0a572a7b99a
dotrungduc <duc010298@gmail.com>
2020-03-28 07:41:13 +0700

    init

---
+++ server/src/main/java/com/github/duc010298/shopxml/ShopXmlApplication.java
@@ -0,0 +1,13 @@
+package com.github.duc010298.shopxml;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ShopXmlApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ShopXmlApplication.class, args);
+    }
+
+}
---
+++ server/src/test/java/com/github/duc010298/shopxml/ShopXmlApplicationTests.java
@@ -0,0 +1,13 @@
+package com.github.duc010298.shopxml;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ShopXmlApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
